0eae8a674e1e2deb33f53d3faf6e1c1d06dd5267
cdmac94 <collinmacleod79@gmail.com>
2021-12-16 12:29:47 +0000

    Add working counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+const text =  document.getElementById("tweet-text");
+const charLeft = document.getElementById("counter");
+const max_chars = 140;
+
+text.addEventListener("input", () => {
+  const remaining = max_chars - text.value.length;
+  charLeft.textContent = `${remaining}`;
+  if (charLeft.textContent < 0) {
+    charLeft.id = "neg-counter";
+  }
+})
+})
