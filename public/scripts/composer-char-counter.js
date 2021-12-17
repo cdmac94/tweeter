41d28b542f108360c6d4b6f4bb5ccba9e3c5a4c8
cdmac94 <collinmacleod79@gmail.com>
2021-12-17 00:10:31 +0000

    Add new tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
 
-const text =  document.getElementById("tweet-text");
-const charLeft = document.getElementById("counter");
-const max_chars = 140;
+  const text =  document.getElementById("tweet-text");
+  const charLeft = document.getElementById("counter");
+  const max_chars = 140;
 
-text.addEventListener("input", () => {
-  const remaining = max_chars - text.value.length;
-  charLeft.textContent = `${remaining}`;
-  if (charLeft.textContent < 0) {
-    charLeft.id = "neg-counter";
-  }
-})
-})
+  text.addEventListener("input", () => {
+    const remaining = max_chars - text.value.length;
+    charLeft.textContent = `${remaining}`;
+    if (charLeft.textContent < 0) {
+      charLeft.id = "neg-counter";
+    } else if (charLeft.textContent > 0) {
+      charLeft.id = "counter";
+    }
+  })
+});
